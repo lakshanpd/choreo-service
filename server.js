35ba122d19178f06db2a78295e5e53dfc9c602e6
lakshanpd <lakshanpd.cse21@gmail.com>
2025-01-28 18:29:39 +0530

    service created

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+    res.send({ message: "Welcome to the Node.js Service!" });
+});
+
+app.get("/data", (req, res) => {
+    res.json({ success: true, data: ["Item1", "Item2", "Item3"] });
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
